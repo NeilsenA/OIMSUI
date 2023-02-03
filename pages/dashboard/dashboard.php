d040cb72b04a2e002a7f1b9843ed19fa2ee03f6e
AlbertoNeilsen <201911902@gordoncollege.edu.ph>
2023-02-03 12:40:13 +0800

    1ST

---
+++ pages/dashboard/dashboard.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '../head.php'; ?>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php include '../navbar.php'; ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <?php include '../sidebar.php'; ?>
+      <!-- partial -->
+      <div class="main-panel" >
+        <div class="content-wrapper" style="background-color:#bddcff;">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+            </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Sales This Month</p>
+                  <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                    <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">P 4,040.00</h3>
+                    <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
+                  </div>  
+                  <p class="mb-0 mt-2 text-danger"><span class="text-black ml-1"><small>This Month</small></span></p>
+                </div>
+              </div>
+            </div>
+              <div class="col-md-4 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Low Stock</p>
+                  <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                    <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">0</h3>
+                    <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
+                  </div>  
+                  <p class="mb-0 mt-2 text-danger"><span class="text-black ml-1"><small>This Month</small></span></p>
+                </div>
+              </div>
+            </div>
+              <div class="col-md-4 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Total Orders</p>
+                  <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                    <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">10</h3>
+                    <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
+                  </div>  
+                  <p class="mb-0 mt-2 text-danger"><span class="text-black ml-1"><small>This Month</small></span></p>
+                </div>
+              </div>
+            </div>
+              <!-- <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Adjustment Table</p>
+                  <div class=" flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                      <table id='example1' class="table table-hover" >
+                          <thead style="font-size:10px">
+                              <tr>
+                                  <th>Product</th>
+                                  <th>Quantity</th>
+                                  <th>Encoded by</th>
+                                  <th>Date Encoded</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr>
+                                  
+                                  <td>Data</td>
+                                  <td>Data</td>
+                                  <td>Data</td>
+                                  <td>Data</td>
+                              </tr>
+                          </tbody>
+                      </table>
+                  </div>  
+                </div>
+              </div>
+            </div> -->
+              <!-- <div class="col-md-4 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Predictive Days</p>
+                  <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                    <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">35 Days</h3>
+                    <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
+                  </div>  
+                  <br>
+                    <button type="button" class="btn btn-primary btn-icon-text btn-flat btn-sm" data-toggle="modal" data-target="#set-days">
+                          Set Predictive Days
+                      </button>
+                </div>
+              </div> -->
+            <!-- </div>
+              <div class="col-md-8 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Low Stock Items</p>
+                  <div class=" flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                      <table class="table table-hover" >
+                          <thead style="font-size:10px">
+                              <tr>
+                                  <th>Product Code</th>
+                                  <th>Product Name</th>
+                                  <th>Inventory Count</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr>
+                                  
+                                  <td>Data</td>
+                                  <td>Data</td>
+                                  <td>Data</td>
+                              </tr>
+                          </tbody>
+                      </table>
+                  </div>  
+                </div>
+              </div>
+            </div> -->
+            
+          </div>
+        </div>
+          <?php include '../modals.php'; ?>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+          <?php include '../footer.php'; ?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+<?php include '../scripts.php'; ?>
+</body>
+
+</html>
+
---
+++ pages/sidebar.php
@@ -0,0 +1,54 @@
+      <!-- partial:../../partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar" style="background-color:#bddcff;">
+          <ul class="nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="../dashboard/dashboard.php">
+                      <i class="ti-shield menu-icon"></i>
+                      <span class="menu-title">Dashboard</span>
+                  </a>
+              </li>
+             
+              
+              <li class="nav-item">
+                  <a class="nav-link" href="../brand/brand.php">
+                      <i class="ti-layers menu-icon"></i>
+                      <span class="menu-title">Brand</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="../category/category.php">
+                      <i class="ti-layers-alt menu-icon"></i>
+                      <span class="menu-title">Categories</span>
+                  </a>
+              </li>
+              
+              <li class="nav-item">
+                  <a class="nav-link" href="../product/product.php">
+                      <i class="ti-shopping-cart menu-icon"></i>
+                      <span class="menu-title">Products</span>
+                  </a>
+              </li>
+
+              <li class="nav-item">
+                  <a class="nav-link" href="../invoice/invoice.php">
+                      <i class="ti-shopping-cart-full menu-icon"></i>
+                      <span class="menu-title">Invoice</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="../sales/sales.php">
+                      <i class="ti-bar-chart-alt menu-icon"></i>
+                      <span class="menu-title">Sales</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="../users/users.php">
+                      <i class="ti-user menu-icon"></i>
+                      <span class="menu-title">Users</span>
+                  </a>
+              </li>
+             
+             
+          </ul>
+      </nav>
+      <!-- partial -->
